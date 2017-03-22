70a1c820abfe2295059dd26d595a80404e03fdd0
ThalaHemanth <viruhemanth@gmail.com>
2017-03-22 20:41:52 +0200

    corrections

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var mongoDB  = 'mongodb://hemanthjs:mongoHemanth123!@#@ds159988.mlab.com:59988/hemanthmongo';
+var mongoDB  = 'mongodb://hemanthjs:mongoHemanth123@ds159988.mlab.com:59988/hemanthmongo';
 mongoose.connect(mongoDB || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
