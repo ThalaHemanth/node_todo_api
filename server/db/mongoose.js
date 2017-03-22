8bc05dd7dcadb55fbf3eb7b7f5656a603eda9280
ThalaHemanth <viruhemanth@gmail.com>
2017-03-22 20:14:56 +0200

    updates

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<hemanthjs>:<mongoHemanth123!@#>@ds159988.mlab.com:59988/hemanthmongo' || 'mongodb://localhost:27017/TodoApp');
+var url  = 'mongodb://<hemanthjs>:<mongoHemanth123!@#>@ds159988.mlab.com:59988/hemanthmongo';
+mongoose.connect(url || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
