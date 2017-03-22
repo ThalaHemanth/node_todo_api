619d341d85b5c9472ddc14f7020fe4a6f3188f76
ThalaHemanth <viruhemanth@gmail.com>
2017-03-22 21:15:34 +0200

    corrections

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,12 @@
 var mongoose = require('mongoose');
+require('dotenv/config');
 
 mongoose.Promise = global.Promise;
-var mongoDB  = 'mongodb://hemanthjs:mongoHemanth123@ds159988.mlab.com:59988/hemanthmongo';
+
+const username = process.env.MONGO_USERNAME;
+const username = process.env.MONGO_PASSWORD;
+
+var mongoDB  = 'mongodb://username:password@ds159988.mlab.com:59988/hemanthmongo';
 mongoose.connect(mongoDB || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
