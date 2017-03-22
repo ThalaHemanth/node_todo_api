70118444afb0a16a357be7b7146a81d58a73360e
ThalaHemanth <viruhemanth@gmail.com>
2017-03-23 00:52:49 +0530

    updates

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ require('dotenv/config');
 mongoose.Promise = global.Promise;
 
 const username = process.env.MONGO_USERNAME;
-const username = process.env.MONGO_PASSWORD;
+const password = process.env.MONGO_PASSWORD;
 
 var mongoDB  = 'mongodb://username:password@ds159988.mlab.com:59988/hemanthmongo';
 mongoose.connect(mongoDB || 'mongodb://localhost:27017/TodoApp');
