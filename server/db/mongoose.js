034640e0ffa8a3c16499115534996ad8bb06659f
ThalaHemanth <viruhemanth@gmail.com>
2017-03-23 09:42:26 +0530

    Welcome

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -9,7 +9,7 @@ mongoose.Promise = global.Promise;
 // var MongoDBUrl = encodeURIComponent(`mongodb://${username}:${password}@ds159988.mlab.com:59988/hemanthmongo`);
 
 var mongoDB  = 'mongodb://hemanthjs:mongoHemanth123@ds159988.mlab.com:59988/hemanthmongo';
-mongoose.connect(MongoDBUrl || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(mongoDB || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
